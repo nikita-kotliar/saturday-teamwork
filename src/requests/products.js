1f54f60dd61daaee681970c79c5a1b32d9790d65
Dok6857 <141351808+Dok6857@users.noreply.github.com>
2024-01-13 13:20:45 +0200

    task done

--- src/requests/products.js
+++ src/requests/products.js
@@ -1,7 +1,11 @@
 import apiInstance from '../services/api.js';
 
 export const getProducts = async () => {
-    const response = await apiInstance.get('/products');
-    return response.data;
+  const response = await apiInstance.get('/products');
+  return response.data;
 };
 
+export const getProductsByID = async id => {
+  const response = await apiInstance.get(`/products/${id}`);
+  return response.data;
+};
